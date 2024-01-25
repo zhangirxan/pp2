c4af25a5983de2ba25524503fb91215f94c6b896
Zhangirkhan <a_zhanggirkhan@kbtu.kz>
2024-01-25 09:39:58 +0600

    added multiple file

---
+++ tsis2/lists.py
@@ -0,0 +1,15 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+#2
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+#3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+#4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+#5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
