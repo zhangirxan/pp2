dcbbe08249520160fd350bb1ef243df5724fbd82
Zhangirkhan <a_zhanggirkhan@kbtu.kz>
2024-01-25 09:42:45 +0600

    added file

---
+++ tsis2/lists_6-8.py
@@ -0,0 +1,9 @@
+#6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+#7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+#8
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
