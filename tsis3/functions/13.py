87ada592455b18ebd12612e10d72217b04e4f853
Zhangirkhan <a_zhanggirkhan@kbtu.kz>
2024-02-07 22:29:07 +0600

    bambambaaaaaaaaam

--- tsis3/functions/13.py
+++ tsis3/functions/13.py
@@ -3,12 +3,12 @@ x=random.randint(1,20)
 t=0
 print("Hello! What is your name?")
 name=input()
-print("Well,", name, "I am thinking of a number between 1 and 20. Take a guess.")
+print("Well,", name, ", I am thinking of a number between 1 and 20. Take a guess.")
 while True:
     y=int(input())
     if (y==x):
         t+=1
-        print("Good job, KBTU! You guessed my number in", t, "guesses!")
+        print("Good job,", name,  ", You guessed my number in", t, "guesses!")
         break
     elif(y>x):
         t+=1
